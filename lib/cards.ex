59844816ff7bc5f268f99677e8c61704ce331758
neverbeenthisweeb <rf.ramadhana@gmail.com>
2021-10-25 22:27:42 +0700

    Initial commit

---
+++ lib/cards.ex
@@ -0,0 +1,24 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # List comprehension is internally a mapping function
+    # As it is a mapping function, nested for loop is quite tricky
+    # List in Elixir is a linked list (OH WHY?)
+    for s <- suits, v <- values do
+      "#{v} of #{s}"
+    end
+  end
+
+  # Arity means the number of accepted args
+  # /0 means 0, /1 means 1 arg
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  # The question mark `?` is just a semantic
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
